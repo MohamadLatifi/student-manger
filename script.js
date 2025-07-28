66364b834ef0ac61cd74330e97c30c56071461b4
MohamadLatifi <goofySmart8@gmail.com>
2025-07-28 16:45:01 +0430

    add student function

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const studentsContainer = document.querySelector(".students");
 const nameInput = studentForm["name"];
 const ageInput = studentForm["age"];
 const rollInput = studentForm["roll"];
+
+const students = JSON.parse(localStorage.getItem("students")) || []; 
+
+const addStudent = (name, age, roll) => {
+    students.push({
+        name,
+        age,
+        roll,
+    });
+
+    localStorage.setItem("students", JSON.stringify(students));
+
+    return { name, age, roll };
+};
