328e43a46f4b8a9e7d7517aecda8dc28ac2a1d97
MohamadLatifi <goofySmart8@gmail.com>
2025-07-28 16:08:17 +0430

    DOM Elements

---
+++ script.js
@@ -0,0 +1,6 @@
+// DOM Elements
+const studentForm = document.getElementById("studentForm");
+const studentsContainer = document.querySelector(".students");
+const nameInput = studentForm["name"];
+const ageInput = studentForm["age"];
+const rollInput = studentForm["roll"];
