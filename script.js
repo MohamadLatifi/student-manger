61806d25183f65326b9748d2c5ce3bad3f12a03f
MohamadLatifi <goofySmart8@gmail.com>
2025-07-28 16:47:52 +0430

    createStudentElement function

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ const addStudent = (name, age, roll) => {
 
     return { name, age, roll };
 };
+
+const createStudentElement = ({ name, age, roll }) => {
+    // Create elements
+    const studentDiv = document.createElement("div");
+    const studentName = document.createElement("h2");
+    const studentAge = document.createElement("p");
+    const studentRoll = document.createElement("p");
+
+    // Fill the content
+    studentName.innerText = `Student name:  ${name};`
+    studentAge.innerText = `Student age:  ${age};`
+    studentRoll.innerText = `Student roll:  ${roll};`
+
+    // Add to the DOM
+    studentDiv.append(studentName, studentAge, studentRoll);
+    studentsContainer.appendChild(studentDiv);
+
+    if (students.length === 0) {
+        studentsContainer.style.display = "none";
+    } else {
+        studentsContainer.style.display = "flex";
+    }
+    //   studentsContainer.style.display = students.length === 0 ? "none" : "flex";
+};
+
+studentsContainer.style.display = students.length === 0 ? "none" : "flex";
+
+students.forEach(createStudentElement);
